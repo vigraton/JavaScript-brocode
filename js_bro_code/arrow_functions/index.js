cedd7307e188887e2e301c887179e5aaf190747e
vigraton <viviangraton@gmail.com>
2024-08-23 15:29:24 -0300

    exercises + arrow functions

---
+++ js_bro_code/arrow_functions/index.js
@@ -0,0 +1,28 @@
+// arrow function = a concise way to write function expressions
+// good for  simple functions  that you use only once
+// (parameters) => some code
+
+const hello = (name, age) => {console.log(`Hello ${name}`)
+                            console.log(`You are ${age} years old`)};
+
+hello("Vivian", 19);
+
+//------------------------------------------------------------------------
+
+setTimeout(() => console.log("Bye!"), 3000);
+
+//------------------------------------------------------------------------
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 2 !== 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+
+console.log(squares);
+console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
+console.log(total);
