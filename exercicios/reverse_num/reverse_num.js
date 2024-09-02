b6d98fece1f9d1c401c44c374d69ffaea204034b
vigraton <viviangraton@gmail.com>
2024-09-02 13:27:34 -0300

    reverse_num ex

---
+++ exercicios/reverse_num/reverse_num.js
@@ -0,0 +1,11 @@
+// Write a JavaScript function that reverses a number.
+// Example x = 32243;
+// Expected Output : 34223
+
+let numeros = window.prompt("Digite até 4 números");
+console.log(`${numeros}`);
+
+let inverter = numeros.split('').reverse().join('');
+console.log(`${inverter}`);
+
+
