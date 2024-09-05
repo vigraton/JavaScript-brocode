cd759927d16cafb47458a50ee0dee62412601f99
vigraton <viviangraton@gmail.com>
2024-09-05 18:13:52 -0300

    new ex

---
+++ exercicios/functions_ex/longest_string/index.js
@@ -0,0 +1,3 @@
+// Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
