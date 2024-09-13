72efcd6048acd872520c8876fb3ffa9962742d3b
vigraton <viviangraton@gmail.com>
2024-09-13 14:32:30 -0300

    string concatenada: correção

--- exercicios/functions_ex/type_of/index.js
+++ exercicios/functions_ex/type_of/index.js
@@ -3,10 +3,10 @@ Write a JavaScript function that accepts an argument and returns the type.
 Note : There are six possible values that typeof returns: object, boolean, function, number, string, and undefined.
 */
 
-//let thing;
-
 function displayType(thing){
-    return `${thing}`, typeof thing;
+    return `${thing} is of type ${typeof thing}`;
 }
 
-console.log(displayType(5));
+console.log(displayType('Vivian'));
+console.log(displayType(1.5));
+console.log(displayType(true));
