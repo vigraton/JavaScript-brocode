a50146b922c85078eec120ade628072a6b0c4817
vigraton <viviangraton@gmail.com>
2024-09-16 18:12:09 -0300

    ex 2

---
+++ exercicios/functions_ex/palindrome/index.js
@@ -0,0 +1,4 @@
+/*
+Write a JavaScript function that checks whether a passed string is a palindrome or not?
+A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+*/
