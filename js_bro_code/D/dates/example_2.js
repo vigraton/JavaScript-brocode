1112ea2d2c955fce2ffa5663ca7f143c5b9b5de0
vigraton <viviangraton@gmail.com>
2024-09-20 16:45:59 -0300

    date

---
+++ js_bro_code/D/dates/example_2.js
@@ -0,0 +1,6 @@
+const date1 = new Date("2023-12-31");
+const date2 = new Date("2024-01-01");
+
+if(date2 > date1){
+    console.log('FELIZ ANO NOVO!')
+}
---
+++ js_bro_code/D/dates/index.js
@@ -0,0 +1,22 @@
+/*
+Date objects = Objects that contain values that represent dates and times
+These date objects can be changed and formatted
+*/
+
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDate();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+console.log(dayOfWeek);
