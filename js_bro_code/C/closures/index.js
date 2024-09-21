8478c510b5a800b88278e3e0641cb427573aebab
vigraton <viviangraton@gmail.com>
2024-09-21 08:49:41 -0300

    definition of closure

---
+++ js_bro_code/C/closures/index.js
@@ -0,0 +1,7 @@
+/*
+Closure = A function defined insede of another function,
+the inner function has access to the variables and scope
+os the outer function.
+Allow for private variables and state maintenace
+Used frequently in JS frameworks: React, Vue, Angular
+*/
