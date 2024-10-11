420d1a645aeb95fc5203ca7fad52ae22d8b03410
vigraton <viviangraton@gmail.com>
2024-10-10 21:46:42 -0300

    mouse events final

--- js_bro_code/M/mouse_events/index.js
+++ js_bro_code/M/mouse_events/index.js
@@ -1,7 +1,23 @@
 /*
 evenListener = Listen for specif events to create interactive
 web pages events: click, mouseouver, mouseout
-.addEventListener(event, callback);
+.addEventListener(event, callback, arrow function);
 */
 
 const myBox = document.getElementById("myBox");
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", event => {
+    myBox.style.backgroundColor = "tomato";
+    myBox.textContent = "OUCH!";
+});
+
+myButton.addEventListener("mouseover", event => {
+    myBox.style.backgroundColor = "yellow";
+    myBox.textContent = "Dont do it!";
+});
+
+myButton.addEventListener("mouseout", event => {
+    myBox.style.backgroundColor = "lightgreen";
+    myBox.textContent = "Click me";
+})
